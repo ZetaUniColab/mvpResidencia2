2aa1ff41060fce1ef534aa900bc6e59a9905590d
juliacaastor <jureca58@gmail.com>
2025-10-20 19:37:00 -0300

    mudanca para maven

---
+++ src/test/java/com/easy/chatbot/chatbot_backend/ChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.chatbot.chatbot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
